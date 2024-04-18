10c84d46d49b58abaa959d73ab8890db8790e7bb
mouayo15 <mouadilayoub15@gmail.com>
2024-04-18 16:16:42 +0200

    Refactoring: Ajout de gestion de l'encodage pour la lecture et l'écriture des fichiers

--- python/golden_master_test.py
+++ python/golden_master_test.py
@@ -1,13 +1,16 @@
-import unittest
 import os
+import unittest
 from tennis6 import TennisGame6
 
 class GoldenMasterTest(unittest.TestCase):
+    #windows
     DIR = "./golden-master"
-    LANG ='en'
+    LANG = 'en'
+    #LANG = 'fr'
+
     @staticmethod
     def play_game(p1Points, p2Points, p1Name, p2Name, lang=LANG):
-        game = TennisGame6(p1Name, p2Name,lang)
+        game = TennisGame6(p1Name, p2Name, lang)
         for i in range(max(p1Points, p2Points)):
             if i < p1Points:
                 game.won_point(p1Name)
@@ -23,24 +26,25 @@ class GoldenMasterTest(unittest.TestCase):
         os.makedirs(f"{self.DIR}/{self.LANG}/", exist_ok=True)
 
         # Enregistrement des résultats de chaque partie dans des fichiers
-        for score_player_1 in list(range(0, 16)):
-            for score_player_2 in list(range(0, 16)):
+        for score_player_1 in range(16):
+            for score_player_2 in range(16):
                 with self.subTest(f"{score_player_1}, {score_player_2}"):
-                    sortie = self.play_game(score_player_1, score_player_2, "player1", "player2")
-                    file = open(self.make_file_name(score_player_1, score_player_2), "w")
-                    file.writelines(sortie)
-                    file.close()
+                    score = self.play_game(score_player_1, score_player_2, "player1", "player2")
+                    file_path = self.make_file_name(score_player_1, score_player_2)
+                    with open(file_path, "w") as file:
+                        file.writelines(score)
 
     def test_replay(self):
         # Vérification que les résultats rejoués correspondent aux enregistrements
-        for score_player_1 in list(range(0, 16)):
-            for score_player_2 in list(range(0, 16)):
+        for score_player_1 in range(16):
+            for score_player_2 in range(16):
                 with self.subTest(f"{score_player_1}, {score_player_2}"):
-                    sortie = self.play_game(score_player_1, score_player_2, "player1", "player2")
-                    file = open(self.make_file_name(score_player_1, score_player_2), "r")
-                    attendu = file.read()
-                    file.close()
-                    self.assertEqual(attendu, sortie)
+                    expected_file_path = self.make_file_name(score_player_1, score_player_2)
+                    # utiliser 'with' pour garantir que les fichiers sont correctement fermés même en cas d'erreur.
+                    with open(expected_file_path, "r",encoding="latin-1") as expected_file:
+                        expected_score = expected_file.read()
+                    actual_score = self.play_game(score_player_1, score_player_2, "player1", "player2")
+                    self.assertEqual(expected_score, actual_score)
 
 if __name__ == '__main__':
     unittest.main()
