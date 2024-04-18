b019afd5d0dc8376cc21a8a6d4dd000b668a7981
mouayo15 <mouadilayoub15@gmail.com>
2024-04-18 16:04:26 +0200

    Utilisation d'une constante pour la langue et ajout de la prise en charge de différents langages

--- python/golden_master_test.py
+++ python/golden_master_test.py
@@ -3,10 +3,10 @@ from tennis6 import TennisGame6
 
 class GoldenMasterTest(unittest.TestCase):
     DIR = "./golden-master"
-
+    LANG ='en'
     @staticmethod
-    def play_game(p1Points, p2Points, p1Name, p2Name):
-        game = TennisGame6(p1Name, p2Name)
+    def play_game(p1Points, p2Points, p1Name, p2Name, lang=LANG):
+        game = TennisGame6(p1Name, p2Name,lang)
         for i in range(max(p1Points, p2Points)):
             if i < p1Points:
                 game.won_point(p1Name)
@@ -15,7 +15,7 @@ class GoldenMasterTest(unittest.TestCase):
         return game.score()
 
     def make_file_name(self, score_player_1, score_player_2):
-        return f"{self.DIR}/{score_player_1}_{score_player_2}.txt"
+        return f"{self.DIR}/{self.LANG}/{score_player_1}_{score_player_2}.txt"
 
     def _test_record(self):
         for score_player_1 in list(range(0, 16)):
--- python/tennis6.py
+++ python/tennis6.py
@@ -1,14 +1,22 @@
-# -*- coding: utf-8 -*-
-
 class TennisGame6:
     # Utilisation d'une liste SCORE_NAMES pour stocker les noms des scores, ce qui élimine la duplication de code.
-    SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
+    SCORE_NAMES = {
+        "Love": {"en": "Love", "fr": "Zéro"},
+        "Fifteen": {"en": "Fifteen", "fr": "Quinze"},
+        "Thirty": {"en": "Thirty", "fr": "Trente"},
+        "Forty": {"en": "Forty", "fr": "Quarante"},
+        "Deuce": {"en": "Deuce", "fr": "Égalité"},
+        "All": {"en": "All", "fr": "Partout"},
+        "Advantage": {"en": "Advantage", "fr": "Avantage"},
+        "Win for": {"en": "Win for ", "fr": "Victoire pour "}
+    }
 
-    def __init__(self, player1_name, player2_name):
+    def __init__(self, player1_name, player2_name,lang):
         self.player1Name = player1_name
         self.player2Name = player2_name
         self.player1Score = 0
         self.player2Score = 0
+        self.lang = lang  # Nouveau: Choix de la langue
 
     def won_point(self, player_name):
         if player_name == "player1":
@@ -29,7 +37,7 @@ class TennisGame6:
 
     def _tie_score(self):
         if self.player1Score < 3:
-            return self.SCORE_NAMES[self.player1Score] + "-All"
+            return self.translate(self._score_to_string(self.player1Score)) + "-" + self.translate("All")
         else:
             return "Deuce"
 
@@ -37,10 +45,25 @@ class TennisGame6:
         score_diff = self.player1Score - self.player2Score
         if abs(score_diff) == 1:
             leader = self.player1Name if score_diff == 1 else self.player2Name
-            return "Advantage " + leader
+            return self.translate("Advantage") + leader
         else:
             winner = self.player1Name if score_diff >= 2 else self.player2Name
-            return "Win for " + winner
+            return self.translate("Advantage") + winner
 
     def _regular_score(self):
-        return self.SCORE_NAMES[self.player1Score] + "-" + self.SCORE_NAMES[self.player2Score]
+        return self.translate(self._score_to_string(self.player1Score)) + "-" + self.translate(self._score_to_string(self.player2Score))
+
+    def translate(self, term):
+        # Retourne la traduction du terme dans la langue choisie
+        return self.SCORE_NAMES[term][self.lang]
+
+    @staticmethod
+    def _score_to_string(score):
+        if score == 0:
+            return "Love"
+        elif score == 1:
+            return "Fifteen"
+        elif score == 2:
+            return "Thirty"
+        elif score == 3:
+            return "Forty"
